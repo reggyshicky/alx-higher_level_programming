ebac86e091b673a9095c5ee0de021cf26e85d6d4
reggyshicky <reginahshikanda@gmail.com>
2023-05-09 05:55:58 -0700

    fixing pycodestyle issues

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for t in range(0, 99):
+    print(f"{t:d} = 0x{t:0x}")
---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for v in range(0, 100):
+    if v < 99:
+        print(f"{v:02d}, ", end='')
+    else:
+        print(f"{v:02d}")
