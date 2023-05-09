9c1148814c9d9087479b93e5a4b3050c4fbf1f60
reggyshicky <reginahshikanda@gmail.com>
2023-05-09 07:13:58 -0700

    function that checks for lowercase character.

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,7 @@
 def islower(c):
     codeAscii = ord(c)
 
-    if (codeAscii >=97 and codeAscii <= 122):
+    if (codeAscii >= 97 and codeAscii <= 122):
         return True
     else:
         return False
