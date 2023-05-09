16db38e16ff0e85637981705a0a0dd9bcd5e7aaa
reggyshicky <reginahshikanda@gmail.com>
2023-05-09 08:40:31 -0700

    the fizzbuzz question

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for k in range(1, 101):
+        if k % 3 == 0:
+            print("Fizz", end='')
+        elif k % 5 == 0:
+            print("Buzz", end='')
+        elif k % 3 == 0 and k % 5 == 0:
+            print("fizzBuzz", end='')
+        else:
+            print(k, end='')
+        if k != 100:
+            print(" ", end='')
