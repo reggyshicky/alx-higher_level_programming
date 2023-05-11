6300ccf4384fc4ade6efd7e078eba143e3f01a94
reggyshicky <reginahshikanda@gmail.com>
2023-05-11 02:55:08 -0700

    program that imports a function add from file ad_0.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python
 from add_0 import add
 
-a = 1
-b = 2
-
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+if __name__ = __main__:
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
