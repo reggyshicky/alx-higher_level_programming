2ebae85f6b34d30dfbe9053cdb41eed3865d6a88
reggyshicky <reginahshikanda@gmail.com>
2023-05-11 06:30:26 -0700

    program that improrts all functions fro a file and handles basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(sys.argv[1])
+    operator = (sys.argv[2])
+    b = int(sys.argv[3])
+
+    if operator == "+":
+        solution = add(a, b)
+    elif operator == "-":
+        solution = sub(a, b)
+    elif operator == "*":
+        solution = mul(a, b)
+    elif operator == "/":
+        solution = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, _, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, operator, b, solution))
