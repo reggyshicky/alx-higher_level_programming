06115d582e4499e9b7e7d150e650d4b456213ce8
reggyshicky <reginahshikanda@gmail.com>
2023-05-30 14:52:27 +0000

    defining a class square with a private instance, raising TypeError and ValueError

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -20,8 +20,8 @@ class Square():
         """
 
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
 
         self.__size = size
