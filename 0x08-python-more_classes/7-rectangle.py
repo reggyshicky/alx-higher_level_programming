16d9fdb10b4073ce3805b74a687a988ff7f272c0
reggyshicky <reginahshikanda@gmail.com>
2023-06-05 06:09:04 -0700

    stored the string "#" in a public instance attribute and printed it out using__str__

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -30,12 +30,13 @@ class Rectangle():
     def height(self):
         return self.__height
 
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height -= value
+        self.__height = value
 
     def area(self):
         return self.__width * self.__height
