3eac8275bb46265c28d9752ade7a4d333d0c1973
reggyshicky <reginahshikanda@gmail.com>
2023-06-12 03:41:54 -0700

    use of isinstance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module documentation for using isinstance"""
+
+
+def is_same_class(obj, a_class):
+    """defines use if instance"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
