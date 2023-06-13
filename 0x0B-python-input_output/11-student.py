9c69b58356bdecaacde6b68e51a339eec9db0bfe
reggyshicky <reginahshikanda@gmail.com>
2023-06-13 13:28:54 -0700

    Student to disk and reload

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,10 +8,10 @@ class Student():
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self, attrs=None):
         """
-        returns the dictionary representation 
+        returns the dictionary representation
         of student instance
         """
         try:
