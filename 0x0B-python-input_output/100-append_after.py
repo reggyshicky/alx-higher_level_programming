7decee26b350d3864b0c429ba88ea7896049ffa1
reggyshicky <reginahshikanda@gmail.com>
2023-06-13 14:41:03 -0700

    function that inserts a line of text to a file, after each line containing a specific string

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""module documentation"""
+"""module documentation for insertion of a text in a file"""
 
 
-def append_after(filename="", search_string+"", new_string""):
+def append_after(filename="", search_string="", new_string""):
     """
     function that inserts a line of  text to a file after
     each line containing a specific string
