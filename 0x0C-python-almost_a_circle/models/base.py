5416999311245fa5516bacd0520efeed865254e2
reggyshicky <reginahshikanda@gmail.com>
2023-06-17 18:28:03 +0300

    return of json string of the list_dictionaries, use of dumps

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,9 @@
 """module documentation for a class Base"""
 
 
+import json
+
+
 class Base():
     """defines a class called Base"""
     __nb_objects = 0
@@ -13,3 +16,11 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns the json string"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
