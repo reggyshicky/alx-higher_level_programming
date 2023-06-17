5d1323f3a648697c01ac1d950c579e48d440954a
reggyshicky <reginahshikanda@gmail.com>
2023-06-17 13:35:11 +0300

    subclass square inheriting from class Rectangle being created

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module documentation for the class Square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines the class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes the class Square"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """retrieves the width/size of square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets the value of the size/width/height"""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
+                self.id, self.x, self.y, self.width)
