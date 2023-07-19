04f77af6e42d02edf9fa2d80c821b136d7abbf5f
reggyshicky <reginahshikanda@gmail.com>
2023-07-20 02:17:59 +0300

    lists all comedy shows

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -14,7 +14,7 @@ WHERE tv_genres.name NOT IN (
 	 ON tv_shows.id = tv_show_genres.show_id
 
 	 JOIN tv_genres
-	 ON tv_show_genres.genres_id = tv_genres.id
+	 ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY tv_genres.name ASC;
