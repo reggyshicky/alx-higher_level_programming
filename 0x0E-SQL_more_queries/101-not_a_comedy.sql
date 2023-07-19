251a2dc9f7aaeec3fc506ca12b85c4467f09ead0
reggyshicky <reginahshikanda@gmail.com>
2023-07-20 02:27:08 +0300

    lists all comedy shows

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,13 +8,13 @@ SELECT DISTINCT tv_shows.title
        LEFT JOIN tv_genres
        ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title NOT IN (
-    SELECT tv_ahows.title
+    SELECT tv_shows.title
       FROM tv_shows
            JOIN tv_show_genres
 	   ON tv_shows.id = tv_show_genres.show_id
 
 	   JOIN tv_genres
-	   ON tv_show-genres.genre_id = tv_genres.id
+	   ON tv_show_genres.genre_id = tv_genres.id
       WHERE tv_genres.name = 'Comedy'
 )
 ORDER BY tv_shows.title ASC;
