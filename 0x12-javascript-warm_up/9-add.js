6499511e082c7514de88b231313bd96c1ceefc78
reggyshicky <reginahshikanda@gmail.com>
2023-08-14 21:03:00 +0300

    addition of two numbers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const { argv } = require('process');
-let a = parseInt(argv[2]);
-let b = parseInt(argv[3]);
-function add(a, b) {
+const a = parseInt(argv[2]);
+const b = parseInt(argv[3]);
+function add (a, b) {
   console.log(a + b);
 }
 add(a, b);
