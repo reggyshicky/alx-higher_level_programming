1d44a7e65eb8a883eb3ac1ff06b5650cdb1e8412
reggyshicky <reginahshikanda@gmail.com>
2023-08-14 20:08:08 +0300

    array in js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const line of lines) {
-  console.log(line);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
