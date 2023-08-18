80655e5de66c02fe46fc71c882383392754da206
reggyshicky <reginahshikanda@gmail.com>
2023-08-19 02:10:04 +0300

    created a class that inherited from Base->declarative_Base(), gave the class two attributes/column, passed the data type, nullable or not, primary key or not

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,13 +5,15 @@ Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-import sys
+
 
 Base = declarative_base()
 
 
 class State(Base):
+    """class state"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False,
+                unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
