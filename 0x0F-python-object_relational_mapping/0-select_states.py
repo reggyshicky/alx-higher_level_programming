859e7cf77e6e20e9006257ccf0e74a10376a9a6c
reggyshicky <reginahshikanda@gmail.com>
2023-08-18 10:12:36 +0300

    implemented the use of MySQLdb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,20 +6,22 @@ from sys import argv
 Module that lists all states from the database using MySQL
 """
 
-db = MySQLdb.connect(
-    host="localhost",
-    user=argv[1],
-    password=argv[2],
-    port=3306,
-    db=argv[3]
-    )
+if __name__ == "__main__": 
+    db = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        port=3306,
+        db=argv[3]
+        )
 
-cursor = db.cursor()  # cursor object is used 4 execution of sql queries
-cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
-mydata = cursor.fetchall()
+    cursor = db.cursor()  # cursor object is used 4 execution of sql queries
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+    mydata = cursor.fetchall()
 
-for row in mydata:
-    print(row)
+    for row in mydata:
+        print(row)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
+
