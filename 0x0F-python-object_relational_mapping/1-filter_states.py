d6ef732a141e3e4249d5a903ffa87e7c6cc99b5e
reggyshicky <reginahshikanda@gmail.com>
2023-08-18 10:51:37 +0300

    use of LIKE and wildcard % operate to query countries that are starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+modules that lists all states with a name starting with N
+from the database, connect with MySQLdb
+"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+              ORDER BY states.id ASC")
+    mydata = c.fetchall()
+
+    for row in mydata:
+        print(row)
+
+    c.close()
+    db.close()
