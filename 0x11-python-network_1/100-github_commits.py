4f4b856220dfb77ad328ba7614b78c835b3acb17
reggyshicky <reginahshikanda@gmail.com>
2023-09-03 07:37:28 +0300

    Python script that takes 2 arguments in order to solve this challenge.
    The first argument will be the repository name

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""A scrit that shows the last 10 commits of a user"""
+"""A script that shows the last 10 commits of a user"""
 
 import sys
 import requests
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
             sys.argv[2], sys.argv[1])
 
