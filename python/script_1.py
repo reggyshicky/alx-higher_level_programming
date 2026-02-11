c83bbd8449568fbf23a1e7863b04a924a9d060b6
reggyshicky <reginahshikanda@gmail.com>
2026-02-11 20:55:53 +0300

    University of people python

---
+++ python/script_1.py
@@ -0,0 +1,18 @@
+x = 5
+print(x + 1)
+
+print(4/3 * 3.14 * 5**3)
+
+total_price = 60/100 * 24.95 * 60
+print(total_price)
+
+total_shipping_price = (3 * 1) + (59 * 0.75)
+
+print(total_shipping_price)
+print(total_price + total_shipping_price)
+
+def lord_please():
+    print("Oooh God please guide my steps")
+    print("And forgive me of my sins and trespasses")
+
+lord_please()
